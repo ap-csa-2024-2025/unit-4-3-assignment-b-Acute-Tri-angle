0efa01632663ecbf4611f9f5da76034ba2de8f39
Acute-Tri-angle <pptran106@student.hbuhsd.edu>
2024-11-08 10:51:55 -0800

    v1

--- Main.java
+++ Main.java
@@ -4,14 +4,26 @@ public class Main
 {
   public static void main(String[] args)
   {
+    Scanner sc = new Scanner(System.in);
     // You can test your method here
     // System.out.println(isPrime(1));
     // System.out.println(isPrime(17));
+    System.out.println("type an integer duma");
+    int x = sc.nextInt();
+    System.out.println(isPrime(x));
+    sc.close();
   }
 
   public static boolean isPrime(int N)
   {
-    // TODO: Replace and write your code below
-    return false;
+    if (N == 1){
+      return false;
+    }
+    for (int i = 2; i <= N / 2; i++){
+      if (N % i == 0){
+        return false;
+      }
+    }
+    return true;
   }
 }
